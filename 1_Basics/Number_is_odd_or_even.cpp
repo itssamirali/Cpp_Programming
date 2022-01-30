57fda735f302d4916f2573abb5dd74ec91f9aa8e
samir07dev <samir07ali@gmail.com>
2022-01-30 17:56:01 +0530

    check whether the number is odd or even

---
+++ 1_Basics/Number_is_odd_or_even.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main() {
+
+    int n;
+    cout <<"Enter the number that you want to check\n";
+    cin>>n;
+    if(n%2==0){
+
+        cout <<"Number is Even\n";
+    }
+    else{
+
+        cout <<"Number is Odd\n";
+    }
+        getch();
+}      
