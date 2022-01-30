d41536f34bd73b27881929300bbfc921c00ebcfc
samir07dev <samir07ali@gmail.com>
2022-01-30 17:09:31 +0530

    code update with getch

--- 1_Basics/sum.cpp
+++ 1_Basics/sum.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
+#include<conio.h>
 using namespace std;
  int main(){
 
      int a,b;
      int sum;
-     cout <<"Enter the number that you want ot add"<<endl;
+     cout <<"Enter the number that you want to add"<<endl;
      cin>>a>>b;
      sum = a+b;
      cout <<"The sum of a and b is "<<sum<<endl;
+     getch();
 
  }
