1a08136288fedab14d6c8823369aa3c1bb7b44a0
samir07dev <samir07ali@gmail.com>
2022-01-30 16:10:41 +0530

    find the area of th esquare

---
+++ 1_Basics/area_of_the_square.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int length;
+    int area;
+
+    cout <<"Enter the length of the square"<<endl;
+    cin>>length;
+    area = length*length;
+    cout <<"The area of the square is "<<area<<endl;
+}
+
