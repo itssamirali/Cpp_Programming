f57c6d878f4e5cb70394ee2302c3cf0f3a05f328
samir07dev <samir07ali@gmail.com>
2022-01-30 15:21:58 +0530

    add two numbers by input

---
+++ 1_Basics/sum.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+ int main(){
+
+     int a,b;
+     int sum;
+     cout <<"Enter the number that you want ot add"<<endl;
+     cin>>a>>b;
+     sum = a+b;
+     cout <<"The sum of a and b is "<<sum<<endl;
+
+ }
