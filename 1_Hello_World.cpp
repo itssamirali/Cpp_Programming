0a7193650fa484b006730f34833c944cd4c7d0e0
Samir Ali <samir07ali@gmail.com>
2022-01-30 14:55:29 +0530

    Delete 1_Hello_World.cpp

--- 1_Hello_World.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-   
-   cout <<"Welcome to world of programminggit\n"<<endl;
-   cout<<"Namaste Duniya!:-"<<"\n"<<endl; 
-   cout <<"Hello world!"<<endl;
-}
