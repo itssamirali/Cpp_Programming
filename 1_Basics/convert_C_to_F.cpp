02639aaaf1dae9e37c6f48b2ad8faabeb72a7d9d
samir07dev <samir07ali@gmail.com>
2022-01-30 17:31:43 +0530

    conversion of Farhenhit to Celcius

---
+++ 1_Basics/convert_C_to_F.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main() {
+
+    float F,C;
+    cout <<"Enter the value of Farhenhit"<<endl;
+    cin>>F;
+    C=(F-32)*5/9;
+    cout <<"The value of Farhenhit to Celcius is "<<C<<endl;
+}
