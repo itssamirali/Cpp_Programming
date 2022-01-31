89d0ea94a2ef7628faae5793978976e2f53916be
samir07dev <samir07ali@gmail.com>
2022-01-31 14:27:58 +0530

    Maximun of three numbers

---
+++ 1_Basics/find_the_sum_of_N_natural_numbers.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main() {
+
+    int n;
+    cout <<"Enter the number\n";
+    cin>>n;
+    int i,sum=0;
+    while(i<=n){
+
+        sum = sum+i;
+        i++;
+    }
+    cout <<sum;
+        
+}
+    
