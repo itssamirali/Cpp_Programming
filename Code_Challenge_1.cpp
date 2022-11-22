a49db7893d840f7194817d30a8a82823511f6b70
samir07dev <samir07ali@gmail.com>
2022-11-22 21:52:58 +0530

    Welcome to Cpp

---
+++ Code_Challenge_1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    cout << "My Name is Samir Ali.\nI am 23 Years old\n"
+         << endl;
+    cout << "My Name is Samir Ali." << endl;
+    cout << "I am 23 Years old" << endl;
+}
