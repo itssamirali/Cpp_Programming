6b00ee6ee51f498307368b6b65da43bd9d7ef0cb
samir07dev <samir07ali@gmail.com>
2022-11-24 21:25:01 +0530

    code challenge 2

---
+++ Code_challenge_2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+
+    string countries = "India's";
+    char currency = '$';
+    float ruppes = 2.2;
+    int year = 2015;
+
+    cout << "As economic reforms picked up the pace, " << countries << "GDP grew five-fold to reach US" << currency << " " << ruppes << " trillion in " << year << "(as per IMF estimates)." << endl;
+}
