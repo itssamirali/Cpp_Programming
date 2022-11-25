f6b28daaec00e587c80cbf4724cb557eaa281d34
samir07dev <samir07ali@gmail.com>
2022-11-25 20:07:56 +0530

    code challenge 3

--- practise_2.cpp
+++ practise_2.cpp
@@ -1,46 +1,9 @@
 #include <iostream>
 using namespace std;
-
-int main()
+int mian()
 {
 
-    int n;
-    cin >> n;
-    if (n >= 1 || n >= 9)
-    {
-
-        switch (n)
-        {
-
-        case 1:
-            cout << "one";
-            break;
-        case 2:
-            cout << "two";
-            break;
-        case 3:
-            cout << "three";
-            break;
-        case 4:
-            cout << "four";
-            break;
-        case 5:
-            cout << "five";
-            break;
-        case 6:
-            cout << "six";
-            break;
-        case 7:
-            cout << "six";
-            break;
-        case 8:
-            cout << "eight";
-            break;
-        case 9:
-            cout << "Greater than 9";
-            break;
-        default:
-            cout << "Greater than 9";
-        }
-    }
+    int num[5] = {2, 7, 11, 15};
+    cout << num[0] + num[1];
+    return 0;
 }
