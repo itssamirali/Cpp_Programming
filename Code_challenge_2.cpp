4c43aae8003d78f5eb2bc24ce98bf7edfd7f5ba1
Samir Ali <samir07ali@gmail.com>
2023-01-19 00:08:22 +0530

    Revision

--- Code_challenge_2.cpp
+++ Code_challenge_2.cpp
@@ -1,15 +1,13 @@
 #include <iostream>
-#include <string.h>
-
+#include <String.h>
 using namespace std;
 
 int main()
 {
-
-    string countries = "India's";
+    string country = "India's";
     char currency = '$';
-    float ruppes = 2.2;
+    float dollar = 2.2;
     int year = 2015;
 
-    cout << "As economic reforms picked up the pace, " << countries << "GDP grew five-fold to reach US" << currency << " " << ruppes << " trillion in " << year << "(as per IMF estimates)." << endl;
+    cout <<"As economic reforms picked up the pace," << country <<" GDP grew five-fold to reach US"<<currency<<dollar <<" trillion in "<< year <<" as per IMF estimates";
 }
